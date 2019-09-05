b16659b08d136bfaeb1442a6677778852946c1c1
Nigel Jones <njones@andigital.com>
2019-09-05 20:15:34 +0100

    Add voice list

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ function say() {
 
   window.speechSynthesis.speak(utterance);  
 }
+
+function loadVoices() {
+  const voicesSelect = document.getElementById('voiceselect');
+
+  const voices = window.speechSynthesis.getVoices();
+
+  voices.forEach(voice => {
+    const option = document.createElement('option');
+    option.textContent = `${voice.name} (${voice.lang}) ${voice.default ? ' (default)' : ''}`;
+
+    option.setAttribute('data-lang', voice.lang);
+    option.setAttribute('data-name', voice.name);
+    voicesSelect.appendChild(option);
+  });
+}
+
+loadVoices();
+speechSynthesis.onvoiceschanged = loadVoices;
